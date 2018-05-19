cef1c42503d4c8324da96414010e628a03b27b55
Rahul Suresh <rahul.tech.work@gmail.com>
2018-05-19 21:46:26 +0530

    Initial commit

---
+++ src/app/comp/comp.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp',
+  templateUrl: './comp.component.html',
+  styleUrls: ['./comp.component.css']
+})
+export class CompComponent implements OnInit {
+
+  public name="";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
